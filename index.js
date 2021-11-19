c9e1f4b0d02c389e77ce9bec705503c20dae7cd5
divyapolakam <divyapolakam24@gmail.com>
2021-11-19 11:01:31 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="congrats-card-container">
+    <h1 className="congrats-card-heading"> Congratulations </h1>
+    <div className="congrats-card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="congrats-card-profile-image"
+      />
+      <h1 className="congrats-card-name"> Kiran V </h1>
+      <p className="congrats-card-description">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="congrats-card-watch-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
